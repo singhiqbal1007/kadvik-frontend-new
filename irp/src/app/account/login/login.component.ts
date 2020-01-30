653661f5017ee9e79b49eb4db7c7b7ba5893c4d9
Iqbal Singh <singhiqbal1007@gmail.com>
2020-01-30 23:10:03 +0530

    admin login added

--- irp/src/app/account/login/login.component.ts
+++ irp/src/app/account/login/login.component.ts
@@ -1,62 +1,77 @@
 import { Component, OnInit } from "@angular/core";
 import { AccountService } from "../services/account.service";
 import { Router } from "@angular/router";
+import { AdminService } from "app/admin/services/admin-service.service";
 
 @Component({
-    selector: "app-login",
-    templateUrl: "./login.component.html",
-    styleUrls: ["./login.component.scss"]
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+  private loading: boolean = false;
 
-    private loading:boolean = false;
+  private adminToggle: boolean = false;
 
-    private adminToggle:boolean = false;
+  //classes on alert
+  private alertAdd = {
+    hidden: true
+  };
 
-    //classes on alert
-    private alertAdd = {
-        hidden: true
-    };
+  private loginAdd = {
+    shake: false
+  };
 
-    private loginAdd = {
-        shake: false
-    };
+  //user entered values
+  public email = "";
+  public password = "";
 
-    //user entered values
-    public email = "";
-    public password = "";
+  //injecting auth service
+  constructor(private router: Router, private _accountService: AccountService, private _adminService: AdminService) {}
 
-    //injecting auth service
-    constructor(
-        private router: Router,
-        private _accountService: AccountService
-    ) {}
+  ngOnInit() {
+    console.log(this.adminToggle);
+  }
 
-    ngOnInit() {
-        console.log(this.adminToggle)
+  //login function
+  public login() {
+    this.loginAdd.shake = false;
+    this.loading = true;
+    if (this.adminToggle === false) {
+      this._accountService.authenticate(this.email, this.password).subscribe(
+        data => {
+          if (data == false) {
+            //if invalid details
+            this.password = "";
+            this.alertAdd.hidden = false;
+            this.loginAdd.shake = true;
+            this.loading = false;
+            return;
+          }
+          //if details are correct i.e data has some values
+          sessionStorage.setItem("prn", data);
+          this.router.navigate(["/student/notifications"]);
+        },
+        err => {}
+      );
+    } else {
+      this._adminService.authenticate(this.email, this.password).subscribe(
+        data => {
+            console.log(data);
+          if (data == false) {
+            //if invalid details
+            this.password = "";
+            this.alertAdd.hidden = false;
+            this.loginAdd.shake = true;
+            this.loading = false;
+            return;
+          }
+          //if details are correct i.e data has some values
+          sessionStorage.setItem("adminId", data);
+          this.router.navigate(["/admin"]);
+        },
+        err => {}
+      );
     }
-
-    //login function
-    public login() {
-        this.loginAdd.shake = false;
-        this.loading = true;
-        this._accountService.authenticate(this.email, this.password).subscribe(
-            data => {
-                if (data == false) {
-                   //if invalid details
-                    this.password = "";
-                    this.alertAdd.hidden = false;
-                    this.loginAdd.shake = true;
-                    this.loading = false;
-                    return;
-                }
-                //if details are correct i.e data has some values
-                sessionStorage.setItem('prn', data);
-                this.router.navigate(["/student/notifications"]);
-            },
-            err => {
-               
-            }
-        );
-    }
+  }
 }
--- irp/src/app/admin/services/admin-service.service.ts
+++ irp/src/app/admin/services/admin-service.service.ts
@@ -7,19 +7,28 @@ import { TimeTable } from 'app/student/timetable/models/TimeTable';
   providedIn: 'root'
 })
 export class AdminService {
-  readonly rootUrl = "http://localhost:8080/api/timetable/getTimeTableAdmin";
-  readonly updateUrl="http://localhost:8080/api/timetable/updateTimeTable";
+
   constructor(private http: HttpClient) {}
 
   public getTimeTable(courseId): Observable<TimeTable[]> {
     let params = new HttpParams();
-    console.log(courseId);
+    //console.log(courseId);
     params=params.set("courseId",courseId);
-    return this.http.get<TimeTable[]>(this.rootUrl,{params});
+    return this.http.get<TimeTable[]>("http://localhost:8080/api/timetable/getTimeTableAdmin",{params});
   }
 
   public updateTimeTable(timetable): any{
-    console.log(timetable);
-    return this.http.post<any>(this.updateUrl,timetable);
+    //console.log(timetable);
+    return this.http.post<any>("http://localhost:8080/api/timetable/updateTimeTable",timetable);
   }
+
+  public authenticate(email, password){
+    var body = {
+      "email":email,
+      "password":password
+    }
+
+    return this.http.post<any>("http://localhost:8080/api/admin/authenticate",body);
+  }
+
 }
